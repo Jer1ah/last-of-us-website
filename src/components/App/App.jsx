35a85f68d59ceacc9dce53db1c00c89ef4a570e8
Jeriah Bowers <jeriah97bowers@yahoo.com>
2020-05-07 00:47:04 -0400

    adding shop section

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Header from '../Header/Header';
 import About from '../About/About';
+import Shop from '../Shop/Shop';
 
 const App = () => {
     return (
         <div>
             <Header />
             <About />
+            <Shop />
         </div>
     );
 };
---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Shop.module.css';
+
+const Shop = () => {
+    return (
+        <div className={styles.shop}>
+            <div className={styles.container}>
+                <h1>CHOOSE YOUR EDITION</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
