1074f464087cd6d1245766add3826623ed217d8d
Jeriah Bowers <jeriah97bowers@yahoo.com>
2020-05-10 13:53:01 -0400

    styling module component

--- src/components/CollectorsEdition/CollectorsEdition.jsx
+++ src/components/CollectorsEdition/CollectorsEdition.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './CollectorsEdition.module.css';
-import image from '../../img/collectors-edition.png';
+import image from '../../img/collectors-edition.png'
+
+import Module from '../Module/Module';
 
 const CollectorsEdition = () => {
+    const [moduleStatus, toggleModule] = useState(false);
+
     return (
         <div className={styles.container}>
             <div className={styles.image}> 
@@ -12,7 +16,10 @@ const CollectorsEdition = () => {
                 <h3>COLLECTOR'S EDITION</h3>
                 <a href="#" 
                    className={styles.preOrderButton}
-                   onClick={(event) => {event.preventDefault()}}
+                   onClick={(event) => {
+                       event.preventDefault();
+                       toggleModule(!moduleStatus);
+                    }}
                 >PRE ORDER</a>
                 <p>For Digital Pre-order instantly recieve a The Last of Us Part II PSN Avatar featuring Ellie's distinct tattoo. Only available during the pre-order period period for The Last of Us Part II.</p>
                 <p>Pre-order and recieve the following special in-game items at launch.</p>
@@ -26,6 +33,9 @@ const CollectorsEdition = () => {
                 </ul>
                 <span>*Supplements required to unlock recipes, skills and upgrades.</span>
             </div>
+            {moduleStatus ? <Module 
+                edition="Collector's Edition"
+                /> : null}
         </div>
     );
 };
--- src/components/Module/Module.jsx
+++ src/components/Module/Module.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import styles from './Module.module.css';
 
+import closeIcon from '../../img/cancel.svg';
+
 const Module = (props) => {
     return (
         <div className={styles.module}>
             <div className={styles.header}>
                 <h1>The Last of Us Part II {props.edition}</h1>
+                <img src={closeIcon} 
+                     alt="Close Icon" 
+                     className={styles.closeIcon}
+                />
             </div>
             <ul className={styles.list}>
                 <li className={styles.listItem}>
