b7ec3b4ecb8e6838a717512099a520e63c927b8e
Jeriah Bowers <jeriah97bowers@yahoo.com>
2020-05-10 10:58:35 -0400

    adding module component

---
+++ src/components/Module/Module.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styles from './Module.module.css';
+
+const Module = (props) => {
+    return (
+        <div className={styles.module}>
+            <div className={styles.header}>
+                <h1>The Last of Us Part II {props.edition}</h1>
+            </div>
+            <ul className={styles.list}>
+                <li className={styles.listItem}>
+                    <img src="#" alt="PlayStation Logo"/>
+                    <h5 className={styles.price}>{props.price}</h5>
+                    <h5 className={styles.status}>Pre-Order</h5>
+                    <a href="#" className={styles.button}>Buy</a>
+                </li>
+                <li className={styles.listItem}>
+                    <img src="#" alt="PlayStation Logo"/>
+                    <h5 className={styles.price}>{props.price}</h5>
+                    <h5 className={styles.status}>Pre-Order</h5>
+                    <a href="#" className={styles.button}>Buy</a>
+                </li>
+                <li className={styles.listItem}>
+                    <img src="#" alt="PlayStation Logo"/>
+                    <h5 className={styles.price}>{props.price}</h5>
+                    <h5 className={styles.status}>Pre-Order</h5>
+                    <a href="#" className={styles.button}>Buy</a>
+                </li>
+                <li className={styles.listItem}>
+                    <img src="#" alt="PlayStation Logo"/>
+                    <h5 className={styles.price}>{props.price}</h5>
+                    <h5 className={styles.status}>Pre-Order</h5>
+                    <a href="#" className={styles.button}>Buy</a>
+                </li>
+                <li className={styles.listItem}>
+                    <img src="#" alt="PlayStation Logo"/>
+                    <h5 className={styles.price}>{props.price}</h5>
+                    <h5 className={styles.status}>Pre-Order</h5>
+                    <a href="#" className={styles.button}>Buy</a>
+                </li>
+            </ul>
+            <div className={styles.footer}></div>
+        </div>
+    );
+};
+
+export default Module;
