f1ff2b0e4784c78323b13645b414bad17f26b105
Jeriah Bowers <jer1ah@AceBoog1es-MacBook-Pro.local>
2020-05-12 14:16:47 -0400

    Updates

---
+++ precache-manifest.eeb2265c62642389bfcdbce45a982160.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44d08173cecbd3c58fe7455ed38525e6",
+    "url": "/last-of-us-website/index.html"
+  },
+  {
+    "revision": "467ba6a64913e0431437",
+    "url": "/last-of-us-website/static/css/main.b0e8c53c.chunk.css"
+  },
+  {
+    "revision": "93eca31d4786ccec81f9",
+    "url": "/last-of-us-website/static/js/2.f0a50c83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/last-of-us-website/static/js/2.f0a50c83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "467ba6a64913e0431437",
+    "url": "/last-of-us-website/static/js/main.ecdc7d6a.chunk.js"
+  },
+  {
+    "revision": "296de939873c7accce56",
+    "url": "/last-of-us-website/static/js/runtime-main.b3ac0491.js"
+  },
+  {
+    "revision": "e9749e1dfd332178a7fa53615b4d1ec3",
+    "url": "/last-of-us-website/static/media/amazon.e9749e1d.png"
+  },
+  {
+    "revision": "d9467835887631cd668af6391c5e8603",
+    "url": "/last-of-us-website/static/media/cancel.d9467835.svg"
+  },
+  {
+    "revision": "269671ff46bf56710c69774eb28436e3",
+    "url": "/last-of-us-website/static/media/collectors-edition.269671ff.png"
+  },
+  {
+    "revision": "6c2661d9116416c89fb525a09149a84a",
+    "url": "/last-of-us-website/static/media/ellie-image.6c2661d9.png"
+  },
+  {
+    "revision": "64f07c30e6fdac9facc8c3098f049679",
+    "url": "/last-of-us-website/static/media/gallery-image1.64f07c30.png"
+  },
+  {
+    "revision": "781ebd90f19970247e8752ad9735fe43",
+    "url": "/last-of-us-website/static/media/gallery-image2.781ebd90.png"
+  },
+  {
+    "revision": "b33d0483b66b9e82cf8f0e06c8d5589e",
+    "url": "/last-of-us-website/static/media/gallery-image3.b33d0483.png"
+  },
+  {
+    "revision": "d5d7b4df282b9b6551e6366817a45c17",
+    "url": "/last-of-us-website/static/media/gallery-image4.d5d7b4df.png"
+  },
+  {
+    "revision": "1647cd8e192133335a9763e5d1b9597c",
+    "url": "/last-of-us-website/static/media/gallery-image5.1647cd8e.png"
+  },
+  {
+    "revision": "f6c6f15a88862bfcfa928ede82923d42",
+    "url": "/last-of-us-website/static/media/gallery-image6.f6c6f15a.png"
+  },
+  {
+    "revision": "32bd7fb026c7703ba5c2c15986cbab5d",
+    "url": "/last-of-us-website/static/media/gamestop.32bd7fb0.png"
+  },
+  {
+    "revision": "36e27dffe0d766ba9259befd4a72ac55",
+    "url": "/last-of-us-website/static/media/header-image.36e27dff.png"
+  },
+  {
+    "revision": "fdee8d17e1d0a4c953542373cfa7caf5",
+    "url": "/last-of-us-website/static/media/info-image1.fdee8d17.png"
+  },
+  {
+    "revision": "3f606d5e533c75564fcfd60adfc84a99",
+    "url": "/last-of-us-website/static/media/info-image2.3f606d5e.png"
+  },
+  {
+    "revision": "decc827f9a93be1c764c7db24a27e103",
+    "url": "/last-of-us-website/static/media/info-image3.decc827f.png"
+  },
+  {
+    "revision": "6d21070cbc5596d48ca2136ddeeae388",
+    "url": "/last-of-us-website/static/media/last-of-us-wallpaper.6d21070c.jpg"
+  },
+  {
+    "revision": "8d56a18fa680e9ddb04ef87965cd92f8",
+    "url": "/last-of-us-website/static/media/menu.8d56a18f.svg"
+  },
+  {
+    "revision": "5ac0bfba6768cab14733382c713278f3",
+    "url": "/last-of-us-website/static/media/navigation-logo.5ac0bfba.png"
+  },
+  {
+    "revision": "0ae7250407c6f15ac1dc6a028b2b5c9f",
+    "url": "/last-of-us-website/static/media/news-image1.0ae72504.png"
+  },
+  {
+    "revision": "1e0643334a6a55f7ba6b7f34ce39b009",
+    "url": "/last-of-us-website/static/media/news-image2.1e064333.png"
+  },
+  {
+    "revision": "4fe1e22602fabc5c7354e1f6992ea65a",
+    "url": "/last-of-us-website/static/media/special-edition.4fe1e226.png"
+  },
+  {
+    "revision": "d307cbdad175a06f383997559db5b898",
+    "url": "/last-of-us-website/static/media/standard-edition.d307cbda.jpg"
+  },
+  {
+    "revision": "2879678a11ed93024d1faefa17c65955",
+    "url": "/last-of-us-website/static/media/target.2879678a.png"
+  },
+  {
+    "revision": "d6d9eeb7ed16599f1fd21f24db3b168c",
+    "url": "/last-of-us-website/static/media/walmart.d6d9eeb7.png"
+  },
+  {
+    "revision": "32236bc466939840f74462b3a3e92bfb",
+    "url": "/last-of-us-website/static/media/woods-bg.32236bc4.png"
+  }
+]);
